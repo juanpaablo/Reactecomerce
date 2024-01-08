f0bdee1f4ab5ac797d7695cceb2d6bb652b68d74
juanpaablo <pablosalazar1@outlook.com>
2024-01-08 14:00:53 -0300

    se agrego una funcion para borrar algo de la lista de productos

--- src/components/ViewProducts.jsx
+++ src/components/ViewProducts.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { ListGroup } from "react-bootstrap";
-import "../css/viewproduct.css"
+import { ListGroup, Button } from "react-bootstrap";
+import { ListGroupItem } from "react-bootstrap"; // Corrige la importación
+import "../css/viewproduct.css";
 
 function ViewProduct() {
   const url = "http://localhost:4555/api/productos";
@@ -17,6 +18,7 @@ function ViewProduct() {
   };
 
   const [list, setList] = useState([]);
+  const [deleteproduct, setdeleteproduct] = useState(null);
 
   useEffect(() => {
     getlist().then((data) => {
@@ -24,15 +26,42 @@ function ViewProduct() {
     });
   }, []); // Empty dependency array means this effect runs once when the component mounts
 
+
   const content = list.map((card) => (
     <div className="container" key={card.producto_id}>
       <div className="listado">
-        {/* Utiliza la clase 'd-flex' para mostrar la lista horizontalmente */}
         <ListGroup horizontal className="listado">
           <ListGroup.Item>{card.producto_name}</ListGroup.Item>
           <ListGroup.Item>{card.producto_categoria}</ListGroup.Item>
           <ListGroup.Item>{card.producto_origen}</ListGroup.Item>
           <ListGroup.Item>{card.producto_price}</ListGroup.Item>
+          <ListGroupItem>
+            <button
+              className="simple"
+              aria-label="borrar"
+              onClick={() => {
+                axios
+                  .delete(url + "/" + card.producto_id)
+                  .then((response) => {
+                    if (response.status === 200) {
+                      // Eliminar el elemento localmente en lugar de recargar la página
+                      const updatedList = list.filter(
+                        (item) => item.producto_id !== card.producto_id
+                      );
+                      setList(updatedList);
+  
+                      alert(card.producto_name + " se borró exitosamente");
+                    }
+                  })
+                  .catch((error) => {
+                    console.error("Error deleting item:", error);
+                    alert("Error al borrar el elemento");
+                  });
+              }}
+            >
+              Borrar
+            </button>
+          </ListGroupItem>
         </ListGroup>
       </div>
     </div>
